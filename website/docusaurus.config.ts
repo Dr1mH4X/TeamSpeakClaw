708c27461eb89ebbd942dc376bfa67a9f84df252
Dr1mH4X <102709153+Dr1mH4X@users.noreply.github.com>
2026-03-28 11:13:23 +0800

    docs(website): 首页浅色模式优化

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -53,21 +53,6 @@ const config: Config = {
           editUrl:
             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ["rss", "atom"],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-          // Useful options to enforce blogging best practices
-          onInlineTags: "warn",
-          onInlineAuthors: "warn",
-          onUntruncatedBlogPosts: "warn",
-        },
         theme: {
           customCss: "./src/css/custom.css",
         },
@@ -94,7 +79,7 @@ const config: Config = {
           position: "left",
           label: "文档",
         },
-        { to: "/blog", label: "博客", position: "left" },
+        { href: "https://dreamhax.cc", label: "博客", position: "left" },
         {
           type: "localeDropdown",
           position: "right",
@@ -107,7 +92,6 @@ const config: Config = {
       ],
     },
     footer: {
-      style: "dark",
       links: [
         {
           title: "Docs",
@@ -140,7 +124,7 @@ const config: Config = {
           items: [
             {
               label: "Blog",
-              to: "/blog",
+              href: "https://dreamhax.cc",
             },
             {
               label: "GitHub",
