a33b5dbe4abf9930b96f79b123b0dc89ca2ddb38
Dr1mH4X <102709153+Dr1mH4X@users.noreply.github.com>
2026-03-30 20:39:37 +0800

    chore: 清理函数

--- src/config/serverquery.rs
+++ src/config/serverquery.rs
@@ -1,4 +1,3 @@
-use super::toml_value;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
@@ -25,24 +24,5 @@ impl Default for SqConfig {
             server_id: 1,
             bot_nickname: "TSClaw".to_string(),
         }
-    }
-}
-
-impl SqConfig {
-    pub fn to_toml(&self) -> String {
-        let mut output = String::new();
-        output.push_str("[serverquery]\n");
-        output.push_str(&format!("host = {}\n", toml_value(&self.host)));
-        output.push_str(&format!("port = {}\n", self.port));
-        output.push_str(&format!("ssh_port = {}\n", self.ssh_port));
-        output.push_str(&format!("method = {}\n", toml_value(&self.method)));
-        output.push_str(&format!("login_name = {}\n", toml_value(&self.login_name)));
-        output.push_str(&format!("login_pass = {}\n", toml_value(&self.login_pass)));
-        output.push_str(&format!("server_id = {}\n", self.server_id));
-        output.push_str(&format!(
-            "bot_nickname = {}\n",
-            toml_value(&self.bot_nickname)
-        ));
-        output
     }
 }
