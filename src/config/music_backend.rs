ac0a91c4d1b34002a539cb9972c93f3e802c89d4
Dr1mH4X <102709153+Dr1mH4X@users.noreply.github.com>
2026-05-09 11:28:36 +0800

    feat: add ncm_api_url and ncm_cookie to MusicBackendConfig

--- src/config/music_backend.rs
+++ src/config/music_backend.rs
@@ -5,6 +5,8 @@ pub struct MusicBackendConfig {
     pub backend: String,
     pub base_url: String,
     pub musicbot_name: String,
+    pub ncm_api_url: String,
+    pub ncm_cookie: String,
 }
 
 impl Default for MusicBackendConfig {
@@ -13,6 +15,8 @@ impl Default for MusicBackendConfig {
             backend: "ts3audiobot".to_string(),
             base_url: "http://127.0.0.1:8009".to_string(),
             musicbot_name: "ts3audiobot".to_string(),
+            ncm_api_url: "http://127.0.0.1:3000".to_string(),
+            ncm_cookie: String::new(),
         }
     }
 }
