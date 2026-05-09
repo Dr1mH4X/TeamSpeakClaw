bd3acef27959db36efb2d5dc5d619fb5359c5501
Dr1mH4X <102709153+Dr1mH4X@users.noreply.github.com>
2026-05-09 10:44:09 +0800

    feat: add musicbot_name to filter self-audio

--- src/config/music_backend.rs
+++ src/config/music_backend.rs
@@ -4,6 +4,7 @@ use serde::{Deserialize, Serialize};
 pub struct MusicBackendConfig {
     pub backend: String,
     pub base_url: String,
+    pub musicbot_name: String,
 }
 
 impl Default for MusicBackendConfig {
@@ -11,6 +12,7 @@ impl Default for MusicBackendConfig {
         Self {
             backend: "ts3audiobot".to_string(),
             base_url: "http://127.0.0.1:8009".to_string(),
+            musicbot_name: "ts3audiobot".to_string(),
         }
     }
 }
